b3b6544d4ec83ca074cec0876a4fbce34dd80f2f
KimYunYeong <61217259+KimYunYeong@users.noreply.github.com>
2022-05-21 12:27:36 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function headerNav(){
+  document.write(
+    '<nav id="headerNav">'+
+      '<ul>'+
+        '<li><a href="./index.html">Home</a></li>'+
+        '<li><a href="">Tap2</a></li>'+
+        '<li><a href="">Tap3</a></li>'+
+        '<li><a href="">Tap4</a></li>'+
+      '</ul>'+
+    '</nav>');
+}
