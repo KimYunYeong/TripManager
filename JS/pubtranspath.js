3bde1b4bd7d5bfdff72ae76bf245ae3e09c0a3b0
ChangHyeon <canghaun@naver.com>
2022-06-15 01:23:56 +0900

    Update pubtranspath.js

--- JS/pubtranspath.js
+++ JS/pubtranspath.js
@@ -310,7 +310,8 @@ pubtranspath.setPathList = function(form, result) {
         path[t].radio.setAttribute("type", "radio");
         path[t].radio.setAttribute("name", "path");
         path[t].radio.setAttribute("value", t);
-        path[t].radio.setAttribute("onclick", "import {pubtranspath}; pubtranspath.onClick(data, data['result']['path'][this.value]);");
+        path[t].radio.setAttribute("onclick", 
+            "import {pubtranspath} from './JS/pubtranspath.js'; pubtranspath.onClick(data, data['result']['path'][this.value]);");
         path[t].label.innerHTML += "<strong>경로 " + (t + 1) + "</strong>";
         path[t].label.appendChild(path[t].details);
         path[t].details.appendChild(path[t].title);
