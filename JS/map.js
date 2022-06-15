54fd39065c9dd0900dd62437a80e95f9e4330d17
ChangHyeon <canghaun@naver.com>
2022-06-16 06:28:39 +0900

    Update map.js

--- JS/map.js
+++ JS/map.js
@@ -238,8 +238,9 @@ $("#btn_select").click(function () {
 	} else { // 대중교통 경로 지도에 표시
 		if (startX && startY && endX && endY) {
 			if (viaPointsList) {
-
+				console.log(1);
 			} else {
+				console.log(0);
 				var pathDiv = document.createElement('div');
 				document.appendChild(pathDiv);
 				pubtranspath.searchPubTransPathAJAX(map, pathDiv, startY, startX, endY, endX);
