d24ef0f6055a7743ac494be983db95337a0f22ba
ChangHyeon <canghaun@naver.com>
2022-06-16 12:42:43 +0900

    Update pubtranspath.js

--- JS/pubtranspath.js
+++ JS/pubtranspath.js
@@ -335,27 +335,27 @@ pubtranspath.setPathList = function(map, element, result, startx, starty, endx, 
         path[t].details.appendChild(path[t].list);
         switch (result["path"][t]["pathType"]) {
             case 1:
-                path[t].summary.innerText = "지하철";
+                path[t].summary.innerText = "지하철 (총 " + result["path"][t]["info"]["totalTime"] + "분)";
                 break;
             case 2:
-                path[t].summary.innerText = "버스";
+                path[t].summary.innerText = "버스 (총 " + result["path"][t]["info"]["totalTime"] + "분)";
                 break;
             case 3:
-                path[t].summary.innerText = "지하철 + 버스";
+                path[t].summary.innerText = "지하철 + 버스 (총 " + result["path"][t]["info"]["totalTime"] + "분)";
                 break;
             case 11:
-                path[t].summary.innerText = "열차";
+                path[t].summary.innerText = "열차 (총 " + result["path"][t]["info"]["totalTime"] + "분)";
                 break;
             case 12:
-                path[t].summary.innerText = "고속 / 시외버스";
+                path[t].summary.innerText = "고속 / 시외버스 (총 " + result["path"][t]["info"]["totalTime"] + "분)";
                 break;
             case 13:
-                path[t].summary.innerText = "항공";
+                path[t].summary.innerText = "항공 (총 " + result["path"][t]["info"]["totalTime"] + "분)";
                 break;
             case 20:
                 path[t].summary.innerText = "시외교통 복합(열차 + 고속버스 등)";
         }
-        path[t].summary.innerText += " (총 " + result["path"][t]["info"]["totalTime"] + "분)";
+        //path[t].summary.innerText += " (총 " + result["path"][t]["info"]["totalTime"] + "분)";
         for (var i = 0; i < result["path"][t]["subPath"].length; i++) {
             switch (result["path"][t]["subPath"][i]["trafficType"]) {
                 case 1: //지하철
