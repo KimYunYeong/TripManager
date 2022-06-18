f0b6d3a7467710a0004a60fe91c32b355f1fd39f
ChangHyeon <canghaun@naver.com>
2022-06-18 12:01:17 +0900

    Update travel.js

--- JS/travel.js
+++ JS/travel.js
@@ -117,6 +117,7 @@ function displayPage(area, xml, page) {
         destination.recommend[i].details.appendChild(destination.recommend[i].summary);
         destination.recommend[i].details.appendChild(destination.recommend[i].image);
         destination.recommend[i].details.appendChild(destination.recommend[i].address);
+        recommendArea.innerHTML += "<br>";
         if (x[i + (10 * (page - 1))])
             destination.longitude[i] = parseFloat(x[i + (10 * (page - 1))].textContent);
         if (y[i + (10 * (page - 1))])
